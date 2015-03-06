339a1fc641ce5b2822f42b6e7daff59034518722
Zhao Yongfeng <zhaoyongfeng@ZhaodeMacBook-Pro.local>
2015-03-06 15:24:29 +0800

    Initial Commit

---
+++ spectral_solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  spectral_solver
+//
+//  Created by Zhao Yongfeng on 15/3/6.
+//  Copyright (c) 2015年 ZYF. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
